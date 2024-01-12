5dd6965bf27defbc8c83ab1a276defa73fb7ea1c
tseng91301 <tseng91301@gmail.com>
2024-01-12 21:38:20 +0800

    rebase, add all program together

---
+++ arduino_code/main/main.ino
@@ -0,0 +1,14 @@
+#include "heat_resistor.h"
+#include "motors.h"
+#include "tds.h"
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(/*RX = */17, /*TX = */16);
+DualMotor engine(41,43,45,47);
+TdsSensor tds1(A0);
+void setup(){
+    engine.enable_EN(39,49);
+}
+void loop(){
+    engine.move();
+
+}
