e4d091f2f19a7fe239421631d79cfd81cdd8a206
tseng91301 <tseng91301@gmail.com>
2024-01-18 09:28:31 +0800

    Add Bluetooth contorl module.

--- main.ino
+++ main.ino
@@ -3,18 +3,24 @@
 #include "tds.h"
 #include "timer.h"
 #include "uarttransfer.h"
+#include "bt_contorl.h"
 #include <SoftwareSerial.h>
 #include <ArduinoJson.h>
-SoftwareSerial BTserial(/*RX = */17, /*TX = */16);
 DualMotor engine(41,43,45,47);
 TdsSensor tds1(A0);
 Timer generate_detector_data;
+BTcontorl contorl1(/*TX = */16, /*RX = */17);
 void setup(){
+    Serial.begin(9600);
     engine.enable_EN(39,49);
     generate_detector_data.add(transfer_data,/*delayTime = */ 200);
 }
 void loop(){
+    generate_detector_data.execute();
     engine.move();
+    if(contorl1.receive()){
+        Serial.println(contorl1.getstr());
+    }
 }
 void transfer_data(){
     DynamicJsonDocument values(200);
@@ -24,7 +30,7 @@ void transfer_data(){
 
     String values_o;
     serializeJson(values, values_o);
-    uartsend(values_o);
+    contorl1.send(values_o);
 }
 
 /*13th commit*/
